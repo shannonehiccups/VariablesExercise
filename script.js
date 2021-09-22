e3ae9c37f09ca427999e8f3da5670eb60e648100
Shannon Hicks <shannonehiccups@gmail.com>
2021-09-22 09:05:50 -0500

    added bonus

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let num=20;
 num=15;
 console.log(num);
-num=15+7;
+num += 7;
 console.log(num);
 num--;
 console.log(num);
@@ -12,3 +12,14 @@ let blank;
 console.log(blank);
 blank =null;
 console.log(blank);
+
+// Bonus 1 
+const quarter=0.25, dime=0.10, nickel=0.05, penny=0.01;
+// Bonus 2
+console.log(quarter, nickel, dime, penny);
+// Bonus 3
+let letter = "qwertyuiop";
+// bonus 4
+console.log(letter[letter.length -1]);
+
+
